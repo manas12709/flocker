0df7c05111824e0fc195f2f1755c6529452d3df5
CyberLord09 <anvay.vahia@gmail.com>
2025-02-04 14:52:46 -0800

    fix url

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -4,7 +4,7 @@ if (location.hostname === "localhost") {
 } else if (location.hostname === "127.0.0.1") {
         pythonURI = "http://127.0.0.1:8505";
 } else {
-        pythonURI =  "https://prism.nighthawkcodingsociety.com";
+        pythonURI =  "https://prism.stu.nighthawkcodingsociety.com";
 }
 export var javaURI;
 if (location.hostname === "localhost") {
